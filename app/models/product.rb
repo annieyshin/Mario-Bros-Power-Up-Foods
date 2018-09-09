f55e9a3b602167d2a0ebac66215f7503f32c47e3
Annie Shin <annieyshin@gmail.com>
2018-09-09 15:03:36 -0700

    add in scope for three most recent products

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,9 @@ class Product < ActiveRecord::Base
   validates :name, :presence => true
   validates :cost, :presence => true
   validates :country_of_origin, :presence => true
+
+  scope :three_most_recent, -> { order(created_at: :desc).limit(3)}
+
+
+
 end
