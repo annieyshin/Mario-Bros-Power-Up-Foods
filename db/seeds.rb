15ed9f0117b761547c9fdcddda15b84a030711a1
Annie Shin <annieyshin@gmail.com>
2018-09-17 13:47:10 -0700

    create fake products and reviews with seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,20 @@
 Product.destroy_all
+Review.destroy_all
 
-50.times do |index|
-  Product.create!(name: Faker::Food.product,
-                        description: Faker::Lorem.sentence(250, false, 0).chop)
+50.times do
+  product = Product.create!(name: Faker::Food.ingredient,
+                  country_of_origin: Faker::Nation.nationality,
+                  cost: Faker::Number.digit
+                )
+  5.times do
+    Review.create!(description: Faker::Lorem.paragraph_by_chars(242),
+                  rating: Faker::Number.between(1, 5),
+                  product_id: product.id
+                  )
+  end
 end
 
+
 p "Created #{Product.count} products"
+
+p "Created #{Review.count} reviews"
