b78fe75780df43baf2a0103d8006d49d2041a837
Annie Shin <annieyshin@gmail.com>
2018-09-17 23:57:41 -0700

    fix most reviews

--- app/models/product.rb
+++ app/models/product.rb
@@ -11,12 +11,12 @@ class Product < ActiveRecord::Base
   scope :three_most_recent, -> { order(created_at: :desc).limit(3)}
 
   scope :most_reviews, -> {(
-  select("products.name, count(reviews.rating) as reviews_count")
-  .joins(:reviews)
-  .group("products.id")
-  .order("reviews_count DESC")
-  .limit(10)
-  )}
+      select("products.id, products.name, products.country_of_origin, count(reviews.id) as reviews_count")
+      .joins(:reviews)
+      .group("products.id")
+      .order("reviews_count DESC")
+      .limit(10)
+      )}
 
 
 end
